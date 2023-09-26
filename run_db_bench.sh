1df0eaecc02b5e3251282d1cabb578f8a4db894d
Amrit Rathie <amrit.rathie@gmail.com>
2023-09-25 21:12:59 -0500

    remove parameter sweep from run_db_bench script

--- run_db_bench.sh
+++ run_db_bench.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Run db_bench with the given mutex and given number of threads and print the results to stdout.
-# Passing multiple integer arguments after number of threads will benchmark the same mutex with different contention levels.
 
 
 CMAKE_BINARY_DIR="${CMAKE_BINARY_DIR:-cmake_build_RelWithDebInfo}"
@@ -17,23 +16,22 @@ mkdir -p results
 
 # run db_bench readrandom with the given mutex and given number of threads
 mutex=$1
-for threads in "${@:2}"; do
-    echo "Running db_bench with $mutex and $threads threads"
-    if [ "$mutex" != "std" ]; then
-        so_path="cmake_build_RelWithDebInfo/locks/lib$mutex.so"
+threads=$2
+echo "Running db_bench with $mutex and $threads threads"
+if [ "$mutex" != "std" ]; then
+    so_path="cmake_build_RelWithDebInfo/locks/lib$mutex.so"
 
-        if [ ! -f "$so_path" ]; then
-            echo "Mutex $mutex not found"
-            continue
-        fi
-
-        preload="$so_path"
-    else
-        preload=""
+    if [ ! -f "$so_path" ]; then
+        echo "Mutex $mutex not found"
+        exit 1
     fi
 
-    LD_PRELOAD="$preload" "$DB_BENCH_PATH" --threads="$threads" --benchmarks=readrandom --use_existing_db=1 --db="$tmpdb" > "./results/$mutex-$threads.txt"
-done
+    preload="$so_path"
+else
+    preload=""
+fi
+
+LD_PRELOAD="$preload" "$DB_BENCH_PATH" --threads="$threads" --benchmarks=readrandom --use_existing_db=1 --db="$tmpdb" > "./results/$mutex-$threads.txt"
 
 # clean up
 echo "Cleaning up $tmpdb"
