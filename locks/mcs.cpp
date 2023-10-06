9df9c250f600fbf42f5229841dd149c5be018e27
Amrit Rathie <amrit.rathie@utdallas.edu>
2023-10-06 13:02:22 -0500

    formatting changes

--- locks/mcs.cpp
+++ locks/mcs.cpp
@@ -1,16 +1,17 @@
 #define LOCK_NAME mcs
+
 #include "interpose.h"
 
 #include <pthread.h>
 
 struct MCSNode {
-  MCSNode *next;
-  bool locked;
+    MCSNode *next;
+    bool locked;
 };
 
 thread_local MCSNode *my_node = new MCSNode;
 
 int REAL(mutex_init)(pthread_mutex_t *mutex,
                      const pthread_mutexattr_t *mutexattr) {
-
+    
 }
